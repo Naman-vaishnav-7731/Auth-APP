10658530658d3cc5c615f95a4ab1c20634784596
namanvaishnav1 <namanvaishnav989@gmail.com>
2025-03-05 10:36:55 +0530

    Implemented logout feature

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,30 @@
-import React from 'react'
-import { Navbar, Container } from 'react-bootstrap'
+import React from 'react';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../redux/authSlice';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.auth.token);
+
   return (
-    <Navbar bg='dark' expand='lg'>
+    <Navbar bg='dark' expand='lg' variant='dark'>
       <Container>
-        <Navbar.Brand href='#home'>
-          <span class='text-warning'>Auth App</span>
+        <Navbar.Brand as={Link} to="/">
+          <span className='text-warning'>Auth App</span>
         </Navbar.Brand>
+        <Nav className="ms-auto">
+          {token && (
+            <Button variant="outline-warning" onClick={() => dispatch(logout())}>
+              Logout
+            </Button>
+          )}
+        </Nav>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
 export default Header;
+
